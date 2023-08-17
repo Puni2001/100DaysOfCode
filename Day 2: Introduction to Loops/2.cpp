91caf63d2382d6e0ee3574c801701ce00d3ee4de
Punith S <111702110+Puni2001@users.noreply.github.com>
2023-08-17 22:57:35 +0530

    Add files via upload
    Signed-off-by: Punith S <111702110+Puni2001@users.noreply.github.com>

---
+++ Day 2: Introduction to Loops/2.cpp
@@ -0,0 +1,12 @@
+// Print Even Numbers between 1 and 20
+
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    // Looping from 1 to 20 
+    for(int i = 1; i <= 20; i++){
+        if(i % 2 == 0){
+            cout<< i <<endl;
+        }
+    }
+}
---
+++ Day 2: Introduction to Loops/3.cpp
@@ -0,0 +1,9 @@
+// Print Multiplication Table of a Number
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i = 1; i <= 10; i++){
+        cout<<" 2 "<<" * "<< i <<" = "<< 2*i <<endl;
+    }
+}
